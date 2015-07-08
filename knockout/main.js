f924d0cf4c36ed16761f06ab4dbe614a9ea25652
crazyyy <crazyyy@gmail.com>
2015-07-08 08:24:27 +0300

    knockout steap 2 ready

--- knockout/main.js
+++ knockout/main.js
@@ -2,6 +2,32 @@ var UserFormModel = function() {
   this.firstName = ko.observable('');
   this.lastName = ko.observable('');
   this.age = ko.observable('');
+  this.sex = ko.observable('');
+  this.adminName = ko.observable('');
+  this.isAdmin = ko.observable(false);
+  this.sexOptions = ['Man', 'Woman'];
+  this.fullName = ko.computed(function (){
+    return this.firstName() + ' ' + this.lastName();
+  },this);
+  this.notEmpty = ko.computed(function () {
+    return !!this.age() || !!this.sex() || !!this.lastName() || !!this.firstName() || !!this.isAdmin();
+  },this);
+  this.reset = function () {
+    this.firstName('');
+    this.lastName('');
+    this.age(null);
+    this.sex(null);
+    this.isAdmin(false);
+  };
+  this.save = function () {
+    event.preventDefault();
+    if (this.notEmpty()) {
+      alert('God save the Queen!')
+    }
+
+  }
+
+
 }
 
 ko.applyBindings(new UserFormModel());

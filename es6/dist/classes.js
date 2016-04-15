73df55e73001a23ffb908cc4d88959ed3468e0a4
crazyyy <crazyyy@gmail.com>
2016-04-15 15:18:37 +0300

    end classes lesson

--- es6/dist/classes.js
+++ es6/dist/classes.js
@@ -10,24 +10,51 @@ var Task = function () {
 
     _classCallCheck(this, Task);
 
-    this.title = title, this.done = false, console.log('Create task ' + this.title + ' ');
+    this.title = title, this._done = false, Task.count += 1, console.log('Create task ' + this.title + ' ');
   }
 
   _createClass(Task, [{
     key: 'complete',
     value: function complete() {
-      this.done = true;
+      this._done = 1;
       console.log('Task ' + this.title + ' is done!');
+    }
+  }, {
+    key: 'done',
+    get: function get() {
+      return this._done === true ? 'Done!' : 'Not done!';
+    },
+    set: function set(value) {
+      if (value !== undefined && typeof value === 'bollean') {
+        this._done = value;
+      } else {
+        console.error('Error!');
+      }
+    }
+  }], [{
+    key: 'getDefaultTitle',
+    value: function getDefaultTitle() {
+      return 'Tasker';
     }
   }]);
 
   return Task;
 }();
 
+Task.count = 0;
+
 var task = new Task('Clean the room');
-var task2 = new Task('Buy a apple');
+// let task2 = new Task('Buy a apple')
+// let task3 = new Task();
 
-console.log(task.title);
-console.log(task2.title);
+// console.log(task.title);
+// console.log(task2.title);
+// console.log(task3.title);
 
-task2.complete();
+// console.log(Task.count);
+
+// task2.complete();
+
+console.log(task.done, task._done);
+task.complete();
+console.log(task.done, task._done);
--- es6/src/classes.js
+++ es6/src/classes.js
@@ -1,21 +1,51 @@
 class Task {
+
   constructor(title = '') {
     this.title = title,
-      this.done = false,
+      this._done = false,
+      Task.count += 1,
       console.log(`Create task ${this.title} `)
   }
 
+  get done() {
+    return this._done === true ? 'Done!' : 'Not done!'
+  }
+  
+  set done(value) {
+    if (value !== undefined && typeof value === 'bollean' ) {
+      this._done = value;
+    } else {
+      console.error('Error!')
+    }
+  }
+
   complete() {
-    this.done = true;
+    this._done = 1;
     console.log(`Task ${this.title} is done!`)
   }
+
+  static getDefaultTitle() {
+    return 'Tasker';
+  }
+
 }
 
+Task.count = 0;
+
 let task = new Task('Clean the room');
-let task2 = new Task('Buy a apple')
+// let task2 = new Task('Buy a apple')
+// let task3 = new Task();
+
+// console.log(task.title);
+// console.log(task2.title);
+// console.log(task3.title);
+
+// console.log(Task.count);
+
+// task2.complete();
 
 
-console.log(task.title);
-console.log(task2.title);
 
-task2.complete();
+console.log(task.done, task._done)
+task.complete()
+console.log(task.done, task._done)
